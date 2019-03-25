73151c67a1a35e069b86696951902d706e27e371
MuzafferOzturk <35779512+MuzafferOzturk@users.noreply.github.com>
2019-03-25 19:40:57 +0300

    add oneSignal

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:demir/Utils/commonUtils.dart';
 import 'package:demir/Utils/color.dart';
 import 'package:demir/Pages/homePage.dart';
+import 'package:onesignal/onesignal.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    OneSignal.shared.init("1750938b-6f66-4fb1-980b-191073e21f2d",iOSSettings: {
+      OSiOSSettings.autoPrompt: false,
+      OSiOSSettings.inAppLaunchUrl: true
+    });
+    OneSignal.shared.setInFocusDisplayType(OSNotificationDisplayType.notification);
+    print("---->>> OneSignal init");
     return MaterialApp(
       title: appCommon.appName,
       theme: new ThemeData(
