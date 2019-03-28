beacebf39db4c01ec43d576dba77bde76c356535
muzafferozturk <muzaffrozturk@gmail.com>
2019-03-28 12:47:00 -0700

    add ios Map

--- lib/controller/controllerContact.dart
+++ lib/controller/controllerContact.dart
@@ -25,10 +25,10 @@ class controllerContact{
         return 'tel:${appCommon.landPhone}';
         break;
       case enumsURL.LOCATION:
-//        if(Platform.isAndroid)
-          return 'https://www.google.com/maps/dir/${appCommon.separator}api=1&origin=&destination=${appCommon.mapAdress}&travelmode=driving';
-//        else
-//          return '';
+        var strReturn = Platform.isAndroid
+            ? 'https://www.google.com/maps/dir/${appCommon.separator}api=1&origin=&destination=${appCommon.mapAdress}&travelmode=driving'
+            : 'https://maps.apple.com/?q=0,0';
+        return strReturn;
         break;
       default:
         return '';
--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:demir/Utils/commonUtils.dart';
 import 'package:demir/Utils/color.dart';
 import 'package:demir/Pages/homePage.dart';
 import 'package:demir/Pages/LoginPage.dart';
-import 'package:onesignal/onesignal.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -11,11 +10,11 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    OneSignal.shared.init("1750938b-6f66-4fb1-980b-191073e21f2d",iOSSettings: {
-      OSiOSSettings.autoPrompt: false,
-      OSiOSSettings.inAppLaunchUrl: true
-    });
-    OneSignal.shared.setInFocusDisplayType(OSNotificationDisplayType.notification);
+//    OneSignal.shared.init("1750938b-6f66-4fb1-980b-191073e21f2d",iOSSettings: {
+//      OSiOSSettings.autoPrompt: false,
+//      OSiOSSettings.inAppLaunchUrl: true
+//    });
+//    OneSignal.shared.setInFocusDisplayType(OSNotificationDisplayType.notification);
     print("---->>> OneSignal init");
     return MaterialApp(
       title: appCommon.appName,
